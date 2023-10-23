647d5c0199a31af67ef51fb35993904e2ebb8f17
bhomik27 <bhomikmaheshwari27@gmail.com>
2023-10-23 23:46:31 +0530

    added different routes for different responses

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 const http = require('http');
 
 
-const server = http.createServer(  (req, res) => {
-    console.log('bhomik');
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-type', 'text/html');
+
+    const url = req.url;
+    if (url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body>');
+        res.write('<h1>Welcome home </h1>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>About</title></head>');
+        res.write('<body>');
+        res.write('<h1>Welcome to About Us page</h1>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Node</title></head>');
+        res.write('<body>');
+        res.write('<h1>Welcome to my Node Js project</h1>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+  
+    
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body>');
+    res.write('<h1>Hello World!</h1>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+
 });
 
-server.listen(4000);
+server.listen(3000);
