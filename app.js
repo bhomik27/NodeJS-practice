f3bf9e33eb5064e06bbb2ace6426aec17239a412
bhomik27 <bhomikmaheshwari27@gmail.com>
2023-10-26 17:43:08 +0530

    addes expressJS, configures middleware functions and sendingh response from one, also used express code for creating server and listning at port 3000

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const http = require("http");
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
-console.log(routes.someText);
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next(); //   this allows the requests to continue in the next middleware
+});
+
+
+app.use((req, res, next) => {
+    console.log("In the middleware 2");
+    res.send('<h1>Hello from express</h1>')
+});
+
+
+// const server = http.createServer(app);
+// server.listen(3000);
+
+//works same as the above code this is done bts in express code
+app.listen(3000);
