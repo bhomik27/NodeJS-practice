006fd9397f60ced5e9d5e12a8e84af20339f5dc8
bhomik27 <bhomikmaheshwari27@gmail.com>
2023-10-30 00:01:38 +0530

    added a navbar, styled all the pages statically by express staatic middleware also made a contact us page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const http = require("http");
 const bodyParser = require('body-parser');
 
@@ -8,14 +10,21 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact');
+const successRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({ extended: false })); // for parsing
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.use('/admin' , adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>");
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 
 //  admin/add-product  => GET request
 router.get('/add-product', (req, res, next) => {
-    res.send('<h1>Add item</h1><form action="/admin/add-product" method="POST"><input type="text" name="title"><br><h1>Item size</h1> <input type="text" name="size"><button type ="submit">Add item</button></form>')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 
